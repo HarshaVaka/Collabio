3ef2d596938621b192b4f5dbbfbd36260021b11e
Sowmya Ralla <e40052094@utcain.com>
2025-11-03 17:12:59 +0530

    Implemented Edit Profile form

--- Frontend/BuddyGoals/src/Pages/Edit.tsx
+++ Frontend/BuddyGoals/src/Pages/Edit.tsx
@@ -6,14 +6,11 @@ export default function Edit({ open, onOpenChange }) {
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/50 backdrop-blur-sm"></Dialog.Overlay>
-        <Dialog.Content>
-          <Dialog.Title>Edit Profile Form</Dialog.Title>
-          <Dialog.Description>
-            <EditProfileForm />
-          </Dialog.Description>
-          <Dialog.Close asChild>
-            <button>close</button>
-          </Dialog.Close>
+        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  bg-white rounded-xl shadow-xl p-6 w-[80%] max-w-2xl">
+          <Dialog.Title className="text-2xl font-bold pb-8 text-green-900">
+            Edit Profile Form
+          </Dialog.Title>
+          <EditProfileForm />
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
